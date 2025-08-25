01d4f6ed93a4bee00ddfb37cb1b24ef5323ebccc
devEducaua <schokito4@gmail.com>
2025-08-24 21:29:24 -0300

    refactor: organize the codebase

--- main.ts
+++ main.ts
@@ -1,63 +1,51 @@
 // import { $ } from "bun";
 import { XMLParser } from "fast-xml-parser";
 
-async function xml(text: string) {
+async function parseLinks(url: string) {
+    const response = await fetch(url);
+    const text = await response.text();
+
     const options = {
         ignoreDeclaration: true
     }
     const parser = new XMLParser(options)
 
     const out = parser.parse(text);
-    // return out;
-
     const channel = out.rss?.channel?.item || out.feed?.entry;
-    // const date = out.rss?.channel?.item?.pubDate || out.rss?.channel?.item?.["dc:date"] || out.feed?.entry?.updated;
-
     const items = Array.isArray(channel) ? channel : [channel];
 
     return [out, items];
 }
 
-async function getUrls(filename: string) {
+async function getFeeds(filename: string) {
     const file = Bun.file(filename);
     const text = await file.text()
     const lines = text.split('\n');
 
-    return lines
-        .map(s => s.trim())
-        .filter(Boolean)
-        .map(s => `https://${s}`);
+    const names = lines
+        .map(l => l.slice(0, l.indexOf(":")))
+        .filter(Boolean);
+
+    const urls = lines
+        .map(l => l.slice(l.indexOf("https://")))
+        .filter(Boolean);
+
+    return [names, urls];
 }
 
-async function getLinks(url: string) {
-    const response = await fetch(url);
+async function logFeeds() {
+    const [names, urls] = await getFeeds("urls.txt");
 
-    const text = await response.text();
+    const result = await Promise.all(urls.map(url => parseLinks(url)));
 
-    const [out, items] = await xml(text);
+    for (let i = 0; i < result.length; i++) {
+        const [out, items] = result[i];
 
-
-    console.log(`----------------${out.rss?.channel?.title || out.feed?.title}----------------`);
-    for (const item of items) {
-        const date = item.pubDate || item["dc:date"] || item.updated;
-        console.log(`${date} - ${item.link}`);
+        console.log(`--------${out.rss.channel.title}--------`);
+        for (const item of items) {
+            console.log(`\x1b[37m${names[i]}: \x1b[0m${item.link}`);
+        }
     }
-
 }
-
-async function returnFeeds() {
-    const urls = await getUrls("urls.txt");
-
-    if (!urls) {
-        console.log("No urls");
-        return;
-    }
-
-    console.log(urls);
-
-    await Promise.all(urls.map(url => getLinks(url)));
-}
-
-returnFeeds();
-
+await logFeeds();
 // await $`xdg-open ${link}`
